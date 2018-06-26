e6aafce19bc03d21267069aa44082ea136f1549a
reneewhite <reneilwesefora@gmail.com>
2018-06-26 14:04:38 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,48 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+value = 0;
+value2= 0;
   constructor(public navCtrl: NavController) {
 
   }
+
+  onClick(){
+    this.value += 3;
+    
+  }
+  onClick1(){
+    this.value += 2;
+    
+  }
+  onClick2(){
+    this.value += 1;
+    
+  }
+  onClick3(){
+    
+    
+    if(this.value)
+    {
+      this.value = 0;
+    }else if(this.value2){
+      this.value2 = 0;
+    }
+  }
+
+  onClick4(){
+    this.value2 += 3;
+    
+  }
+  onClick5(){
+    this.value2 += 2;
+    
+  }
+  onClick6(){
+    this.value2 += 1;
+    
+  }
+ 
+
 
 }
